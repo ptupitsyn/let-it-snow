14a579994890e89a6ec939fe08ec23788b1e882b
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-12-24 19:49:23 +0300

    initial Avalonia test

---
+++ AvaloniaCoreSnow/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Diagnostics;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaCoreSnow
+{
+    class App : Application
+    {
+
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            base.Initialize();
+        }
+
+        static void Main(string[] args)
+        {
+            AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .Start<MainWindow>();
+        }
+
+        public static void AttachDevTools(Window window)
+        {
+#if DEBUG
+            DevTools.Attach(window);
+#endif
+        }
+
+    }
+}
